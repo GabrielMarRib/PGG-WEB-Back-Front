2515c672f411404705fcdaf60e2e70d655349009
Marcos Vinicius <marcosbayblade78@gmail.com>
2024-05-31 20:10:40 -0300

    Pagina do vender produtos 90% feita

--- frontend/src/Pages/App/Services/PagVenderProduto.js
+++ frontend/src/Pages/App/Services/PagVenderProduto.js
@@ -15,42 +15,47 @@ function PagVenderProduto() {
     return (
         <div className="PagVenderProduto">
             <div id="DivForms">
-            <div className='Cabecalho'>
+                <div className='Cabecalho'>
                     <Cabecalho />
                 </div>
                 <div className="container-tela-produtos">
-                    <form className="formulario">
-                        <div className="grupo-input-produto">
-                            <div className="grupo-input">
-                                <label>Selecione um produto:</label>
-                                <select className="controle-formulario" value={produto} onChange={(e) => setProduto(e.target.value)}>
-                                    <option value="">Selecione um produto</option>
-                                </select>
+                    <div className="enquadramento">
+                        <form className="formulario">
+                            <div className="grupo-input-produto">
+                                <div className="grupo-input">
+                                    <label>Selecione um produto:</label>
+                                    <select className="controle-formulario" value={produto} onChange={(e) => setProduto(e.target.value)}>
+                                        <option value="">Selecione um produto</option>
+                                    </select>
+                                </div>
+                                <div className="grupo-input">
+                                    <label>Custo unitário</label>
+                                    <input className="controle-formulario" type="number" value={custoUnitario} readOnly />
+                                </div>
+                                <div className="grupo-input">
+                                    <label>Quantidade disponível</label>
+                                    <input className="controle-formulario" type="number" value={quantidadeDisponivel} readOnly />
+                                </div>
+                                <div className="grupo-input">
+                                    <label>Quantidade de venda</label>
+                                    <input className="controle-formulario" type="number" value={quantidadeVenda} onChange={(e) => setQuantidadeVenda(parseFloat(e.target.value))} />
+                                </div>
+                                <div className="grupo-input">
+                                    <label>Receita estimada</label>
+                                    <input className="controle-formulario" type="number" value={receitaEstimada} readOnly />
+                                </div>
+
+                                <button className="botao" type="button">
+                                    Realizar Venda
+                                </button>
+
+                                <button className="botao" type="button">
+                                    Relatorio de vendas
+                                </button>
+                                
                             </div>
-                            <div className="grupo-input">
-                                <label>Custo unitário</label>
-                                <input className="controle-formulario" type="number" value={custoUnitario} readOnly />
-                            </div>
-                            <div className="grupo-input">
-                                <label>Quantidade disponível</label>
-                                <input className="controle-formulario" type="number" value={quantidadeDisponivel} readOnly />
-                            </div>
-                            <div className="grupo-input">
-                                <label>Quantidade de venda</label>
-                                <input className="controle-formulario" type="number" value={quantidadeVenda} onChange={(e) => setQuantidadeVenda(parseFloat(e.target.value))} />
-                            </div>
-                            <div className="grupo-input">
-                                <label>Receita estimada</label>
-                                <input className="controle-formulario" type="number" value={receitaEstimada} readOnly />
-                            </div>
-                            <button className="botao" type="button">
-                                Calcular Ponto de Pedido
-                            </button>
-                        </div>
-                    </form>
-                </div>
-                <div className="terminal">
-                    {/* Porra pra pesquisa aqui */}
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
