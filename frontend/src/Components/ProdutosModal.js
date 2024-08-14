be98dd48ffb5c36f1769e61646b40b804a67a2e6
GuilhermeTiago22 <guilherme.tiago06@gmail.com>
2024-08-13 22:35:37 -0300

    Telinha em Modal de produtos

--- frontend/src/Components/ProdutosModal.js
+++ frontend/src/Components/ProdutosModal.js
@@ -1,38 +1,68 @@
-import React from 'react'
-import '../Styles/Components/ProdutosModal.css'
+import React, { useState } from 'react';
+import '../Styles/Components/ProdutosModal.css';
+
 function ProdutosModal() {
+  const [abaAtiva, setAbaAtiva] = useState(null);
+
+  const handleClickAba = (nomeAba) => {
+    setAbaAtiva(nomeAba);
+  };
+
   return (
     <div className='ProdutosModal'>
-      <div className='modal-content'>
-        Produtos
-        ATUALIZARa
-        ATUALIZARa
-        ATUALIZARa
-        a
-        ATUALIZARa
-        a
-        a
-
-        ATUALIZARa
-        A
-        A
-        <ul>
-          <li>sexo</li>
-          <li>sexo</li>
-          <li>sexo</li>
-          <li>sexo</li>
-          <li>sexo</li>
-          <li>sexo</li>
-          <li>sexo</li>
-          <li>sexo</li>
-          <li>sexo</li>
-          <li>sexo</li>
-          <li>sexo</li>
-        </ul>
+      <div className='conteudo-modal'>
+        <div className='cabecalho-modal'>
+          <button
+            className={`botao-aba ${abaAtiva === 'Produto' ? 'ativa' : ''}`}
+            onClick={() => handleClickAba('Produto')}
+          >
+            Produto
+          </button>
+          <button
+            className={`botao-aba ${abaAtiva === 'Ponto De Pedido' ? 'ativa' : ''}`}
+            onClick={() => handleClickAba('Ponto De Pedido')}
+          >
+            Ponto De Pedido
+          </button>
+          <button
+            className={`botao-aba ${abaAtiva === 'Curva ABC' ? 'ativa' : ''}`}
+            onClick={() => handleClickAba('Curva ABC')}
+          >
+            Curva ABC
+          </button>
+          <button
+            className={`botao-aba ${abaAtiva === 'Lote Econômico' ? 'ativa' : ''}`}
+            onClick={() => handleClickAba('Lote Econômico')}
+          >
+            Lote Econômico
+          </button>
+          <button className='botao-fechar'>X</button>
+        </div>
+        <div className='corpo-modal'>
+          <ul>
+            <li>SEXO</li>
+            <li>SEXO</li>
+            <li>SEXO</li>
+            <li>SEXO</li>
+            <li>SEXO</li>
+            <li>SEXO</li>
+            <li>SEXO</li>
+            <li>SEXO</li>
+            <li>SEXO</li>
+            <li>SEXO</li>
+            <li>SEXO</li>
+            <li>SEXO</li>
+            <li>SEXO</li>
+            <li>SEXO</li>
+            <li>SEXO</li>
+            <li>SEXO</li>
+            <li>SEXO</li>
+            
+          </ul>
+        </div>
       </div>
     </div>
-
-  )
+  );
 }
 
-export default ProdutosModal
+export default ProdutosModal;
