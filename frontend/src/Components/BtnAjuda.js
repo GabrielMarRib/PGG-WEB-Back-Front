2568b6d3b8b5c2d53c971427cf61f424858f74c8
Marcos Vinicius <marcosbayblade78@gmail.com>
2024-10-14 19:12:25 -0300

    Botão de ajuda já funcional em algumas telas, faltando só adicionar nas outras

--- frontend/src/Components/BtnAjuda.js
+++ frontend/src/Components/BtnAjuda.js
@@ -39,12 +39,128 @@ const BtnAjuda = ({ msgChave, fechar }) => {
         )
       case 'CATEGORIAS':
         return (
-            <>
-              <h1>kkkkk eu sou um teste blz??</h1>
-              <h2>ola sou um h2</h2>
-              <h3>e eu um h3 kkkkkkkkk vai se fuder</h3>
-            </>
+          <> {/* TEM q ter esse baguio vazio englobando tudo (<>) pq o react é chato */}
+          <h1>Guia de Ajuda - Tela de Gerenciamento de Produtos</h1>
+          
+          <h1>Visão Geral</h1>
+          <p>Nesta tela, você pode adicionar novos produtos ao sistema e gerenciar os produtos já cadastrados. É uma maneira eficiente de manter seu estoque sempre atualizado e organizado. Vamos ver como funciona!</p>
+          
+          <h1>Como Usar</h1>
+          
+          <h2>1. Voltar para o Perfil</h2>
+          <p>Se precisar retornar à tela anterior, clique no botão Voltar. Isso te levará de volta à sua página de perfil rapidamente.</p>
+          
+          <h2>2. Adicionando um Produto</h2>
+          <p>Na seção Adicione um produto, preencha os campos obrigatórios marcados com <strong>*</strong>:</p>
+          <ul>
+            <li><strong>Código:</strong> Identificação única do produto.</li>
+            <li><strong>Nome:</strong> Nome do produto.</li>
+            <li><strong>Descrição:</strong> Breve descrição do produto.</li>
+            <li><strong>Código de Barras:</strong> Código de barras do produto.</li>
+          </ul>
+          <p><strong>Informações sobre o Lote:</strong></p>
+          <ul>
+            <li><strong>Fornecedor:</strong> Selecione um fornecedor da lista.</li>
+            <li><strong>Data de Compra:</strong> Insira a data da compra no formato dd/mm/aaaa.</li>
+            <li><strong>Data de Validade:</strong> Insira a data de validade do produto.</li>
+            <li><strong>Quantidade:</strong> Informe a quantidade do produto em estoque.</li>
+            <li><strong>Valor da Compra:</strong> Informe o valor de compra do produto.</li>
+            <li><strong>Valor da Venda:</strong> Informe o preço de venda do produto.</li>
+          </ul>
+          <p>Após preencher todas as informações, clique em <strong>Inserir Produto</strong> para adicionar o produto ao sistema.</p>
+          
+          <h2>3. Pesquisando Produtos Cadastrados</h2>
+          <p>Na seção Produtos cadastrados, você pode pesquisar produtos usando os campos disponíveis:</p>
+          <ul>
+            <li><strong>Pesquisar categoria:</strong> Para encontrar produtos por categoria.</li>
+            <li><strong>Pesquisar produto:</strong> Para localizar um produto específico pelo nome ou código.</li>
+          </ul>
+          
+          <h2>4. Editando Produtos</h2>
+          <p>Ao lado de cada produto cadastrado, existe um botão <strong>Editar produto</strong>. Clique nele se precisar atualizar as informações do produto.</p>
+          
+          <h2>5. Dicas Finais</h2>
+          <p>Sempre revise os dados do produto antes de inserir para evitar erros.</p>
+          <p>Utilize as ferramentas de pesquisa para facilitar a localização de produtos no sistema.</p>
+          <p>Certifique-se de que todas as informações obrigatórias estão preenchidas para que o produto seja cadastrado corretamente.</p>
+          
+        </>
+        
           ) // dispensa break no switch, pq já tem return.
+
+          case 'BAIXASPRODUTOS':
+        return (
+          <> {/* TEM q ter esse baguio vazio englobando tudo (<>) pq o react é chato */}
+          <h1>Guia de Ajuda - Tela de Baixa de Produtos</h1>
+          
+          <h1>Visão Geral</h1>
+          <p>Nesta tela, você pode selecionar um produto de qualquer categoria para efetuar uma baixa. É um processo simples que permite gerenciar o estoque de forma eficaz.</p>
+          
+          <h1>Como Usar</h1>
+          
+          <h2>1. Selecionando um Produto</h2>
+          <p>Na seção "Produtos da categoria acima:", você verá uma lista de produtos disponíveis. Selecione o produto desejado a partir do dropdown. O produto selecionado será exibido logo abaixo.</p>
+          
+          <h2>2. Visualizando os Detalhes do Produto</h2>
+          <div style={{border: '1px solid black', padding: '10px', margin: '20px 0'}}>
+            <p><strong>Produto Selecionado:</strong> Smartphone UltraTech Z10</p>
+            <p><strong>Produto ID:</strong> 33333</p>
+          </div>
+          
+          <h2>3. Verificando a Quantidade Disponível</h2>
+          <p>A quantidade disponível do produto será exibida automaticamente. Certifique-se de que as informações estão corretas antes de prosseguir.</p>
+          
+          <h2>4. Motivo da Baixa</h2>
+          <p>Selecione o motivo da baixa na lista suspensa. Você pode escolher "Expiração/Vencimento" ou outras opções disponíveis.</p>
+          
+          <h2>5. Inserindo a Quantidade da Baixa</h2>
+          <p>Digite a quantidade que você deseja baixar no campo "Quantidade da Baixa". O sistema não permitirá uma baixa superior à quantidade disponível.</p>
+          
+          <h2>6. Motivo da Expiração/Vencimento</h2>
+          <p>Insira o motivo da expiração ou vencimento do produto no campo designado, caso tenha selecionado essa opção.</p>
+          
+          <h2>7. Efetuando a Baixa</h2>
+          <p>Depois de preencher todos os campos necessários, clique no botão "Efetuar Baixa" para processar a baixa do produto selecionado.</p>
+          
+          <h1>Dicas Finais</h1>
+          <p>Antes de efetuar a baixa, sempre verifique se as informações estão corretas. Um pequeno erro pode afetar o controle de estoque.</p>
+          <p>Se precisar voltar e selecionar outro produto, use o botão "Voltar" para retornar à tela anterior.</p>
+        </>
+          ) 
+
+      case 'GERENCIAMENTECATEGORIA':
+        return (
+          <> {/* TEM q ter esse baguio vazio englobando tudo (<>) pq o react é chato */}
+          <h1>Guia de Ajuda - Gerenciamento de Categoria</h1>
+          <h1>Visão Geral</h1>
+          Nesta tela, você pode gerenciar as categorias dos produtos no sistema. Isso facilita a organização e a localização de itens, permitindo uma administração mais eficiente.
+
+          <h1>Como Usar</h1>
+          
+          <h2>1. Voltar para a Página Anterior</h2>
+          Se precisar voltar para a tela anterior, clique no botão Voltar. Isso te levará de volta para a sua página anterior sem perder tempo.
+
+          <h2>2. Adicionando uma Nova Categoria</h2>
+          <p>Clique no botão Adicionar Categoria para abrir o formulário de inserção.</p>
+          <p>Preencha os campos obrigatórios: Nome da Categoria e Descrição. Após isso, clique em "Salvar" para adicionar a categoria ao sistema.</p>
+
+          <h2>3. Listando Categorias</h2>
+          Após adicionar categorias, você verá uma lista delas na tela. Isso te permite visualizar todas as categorias cadastradas e acessar as opções de edição e exclusão.
+
+          <h2>4. Atualizando uma Categoria</h2>
+          <p>Para atualizar uma categoria, clique no ícone de edição ao lado do nome da categoria desejada. Faça as alterações necessárias e clique em "Salvar" para confirmar.</p>
+
+          <h2>5. Deletando uma Categoria</h2>
+          Se decidir que não precisa mais de uma categoria, clique no botão Deletar ao lado da categoria correspondente. O sistema irá perguntar se você tem certeza antes de apagar tudo. É sempre bom verificar duas vezes, né?
+
+          <h1>Dicas Finais</h1>
+          <p>Verifique se o nome da categoria que você está adicionando não está duplicado. Categorias com nomes iguais podem causar confusão.</p>
+          <p>Se precisar editar uma categoria, faça isso rapidamente após a adição, para evitar inconsistências.</p>
+        </>
+
+
+          ) 
+        
       default:
         return (
           <>
--- frontend/src/Pages/App/Services/PagProdutos.js
+++ frontend/src/Pages/App/Services/PagProdutos.js
@@ -13,6 +13,7 @@ import { UserContext } from "../../../Context/UserContext.js";
 
 import ProdutosModal from "../../../Components/ProdutosModal.js";
 import Titulo from "../../../Components/Titulo.jsx";
+import BtnAjuda from "../../../Components/BtnAjuda.js";
 
 function PagProdutos() {
   const UserOBJ = useContext(UserContext); // pega o UserOBJ inteiro, q tem tanto o User quanto o setUser...
@@ -224,6 +225,7 @@ function PagProdutos() {
     console.log(val)
   }
 
+  const [showPopup, setShowPopup] = useState(false); 
 
   return (
     <div className="Produtos">
@@ -236,8 +238,20 @@ function PagProdutos() {
           tituloMsg='Gerenciamento de Produtos' 
         />
 
-        <div className="ajuda"> ? </div>
+        <header className="cabecalhoBtnAjuda">
+          <div className="Botaoajuda" onClick={() => {setShowPopup(true)}}> {/*crie um botão que no onClick faz o setShowPopup ficar true*/}
+            Ajuda
+          </div>
+        </header>
 
+          <div className="BtnAjuda">
+          {showPopup && ( // showPopup && significa: se tiver showPopup (no caso, se for true), faz isso ai embaixo:
+            <BtnAjuda /* chama o btnAjuda */
+              fechar={() => {setShowPopup(false)}} // props do bixo: fechar (passa o setshowPopup como false) (será executado quando a função fechar for chamada no componente btnAjuda)
+              msgChave={"CATEGORIAS"}                   // passa a chave que dita a msg no componente (veja as chaves válidas no componente)
+            />
+          )}
+        </div>
 
         <AlertaNotificação />
 
