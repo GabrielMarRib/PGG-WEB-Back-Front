dc34d8474ec4b1141ce5aa206d2f22a9c68dca4f
GuilhermeTiago22 <guilherme.tiago06@gmail.com>
2024-10-15 16:13:28 -0300

    Ajudei o Marcos a adicionar informações nos quadradinhos da PagHome

--- frontend/src/Pages/App/PagHome.js
+++ frontend/src/Pages/App/PagHome.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect , useContext} from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "../../Styles/App/PagHome.css";
-import { useNavigate, Link} from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import ImageABC from '../../Assets/curvaABC.png';
 import ImageLote from '../../Assets/loteEconomico.png';
 import ImagePEPS from '../../Assets/PEPS.png';
@@ -31,37 +31,82 @@ function PagHome() {
   const [isSelectEstoque, setisSelectEstoque] = useState(false);
   const [isSelectPontoPedido, setisSelectPontoPedido] = useState(false);
   const [isSelectCurvaAbc, setisSelectCurvaAbc] = useState(false);
-  const [userAvatar, setUserAvatar] = useState(null); 
+  const [userAvatar, setUserAvatar] = useState(null);
   const [categorias, setCategorias] = useState([])
+  const [totalCompras, setTotalCompras] = useState(0);
+  const [totalVendas, setTotalVendas] = useState(0);
+
   // ()=> {}
   // function() {}
 
-  useEffect(function(){
+  useEffect(function () {
     const pegaCategorias = async () => { // função existe para separar async do useEffect...
       try {
-          const response = await axios.post('http://pggzettav3.mooo.com/api/index.php', {  // acessa via post (SEMPRE SERÁ POST)                
-              funcao: 'pegacategorias', // dita qual função deve ser utilizada da api. (a gente te fala o nome) // ---> parâmetros da consulta... SÃO necessários.
-              senha: '@7h$Pz!q2X^vR1&K' // teoricamente essa senha tem q ser guardada em um .env, mas isso é trabalho do DEIVYD :)
-          });
-          console.log(response.data) // log para sabermos o que foi pego.
-          setCategorias(response.data)
+        const response = await axios.post('http://pggzettav3.mooo.com/api/index.php', {  // acessa via post (SEMPRE SERÁ POST)                
+          funcao: 'pegacategorias', // dita qual função deve ser utilizada da api. (a gente te fala o nome) // ---> parâmetros da consulta... SÃO necessários.
+          senha: '@7h$Pz!q2X^vR1&K' // teoricamente essa senha tem q ser guardada em um .env, mas isso é trabalho do DEIVYD :)
+        });
+        console.log(response.data) // log para sabermos o que foi pego.
+        setCategorias(response.data)
       } catch (error) {
-          console.log("deu ruim: " + error) // log para sabermos qual foi o erro
+        console.log("deu ruim: " + error) // log para sabermos qual foi o erro
       }
-  };
-  pegaCategorias(); //chama a função
+    };
+    pegaCategorias(); //chama a função
   }, [])
 
+  useEffect(() => {
+    const obterTotais = async () => {
+      try {
+        const response = await axios.post('http://pggzettav3.mooo.com/api/index.php', {
+          funcao: 'ObterTotaisComprasEVendas',
+          senha: '@7h$Pz!q2X^vR1&K'
+        });
+
+        setTotalCompras(response.data.total_compras);
+        setTotalVendas(response.data.total_vendas);
+      } catch (error) {
+        console.log("Erro ao obter totais: " + error);
+      }
+    };
+
+    obterTotais();
+  }, []);
+
   const sections = [
-    { title: "Faturamento por Serviço", content: "Armazenagem: R$ 1.200.000" },
-    { title: "Fornecedores ativos", content: "Lista de fornecedores ativos" },
+    {
+      title: "",
+      content: (
+        <div className="card">
+          <h3 className="card-title">Importar Planilha Excel</h3>
+          <p className="card-text">
+            <span className="destaque-texto">Novo!</span> Importe seus dados de maneira fácil e rápida.
+          </p>
+          <button
+            onClick={() => navigate("/PagUploadExcel")}
+            className="btn-padrao"
+          >
+            Iniciar Importação
+          </button>
+        </div>
+      )
+    },
+    {
+      title: "Valores dos Itens no Armazém",
+      content: (
+        <div>
+          <p>Total de Compras: <br />R$ {new Intl.NumberFormat('pt-BR', { minimumFractionDigits: 2 }).format(totalCompras)}</p>
+          <p>Total de Vendas: <br />R$ {new Intl.NumberFormat('pt-BR', { minimumFractionDigits: 2 }).format(totalVendas)}</p>
+        </div>
+      ),
+    },
     { title: "Capacidade de Armazenagem Utilizada", content: "Utilização dos armazéns: 78%", isSameHeight: true },
-    { title: "Ponto de Pedido", content: <DashboardPP />  },
+    { title: "Ponto de Pedido", content: <DashboardPP /> },
     { title: "Baixas de produto", content: "20 baixas efetuadas hoje" },
     { title: "Satisfação do Cliente", content: "Nível de satisfação dos clientes: 92%" },
     { title: "Evolução de Vendas", content: "Análise Mensal da Evolução de Vendas", isChart: true },
-    
-];
+
+  ];
 
   const buttons = [
     { title: "Estoque", img: ImageCaixa, link: "/PagEscolhaProdutos" },
@@ -125,7 +170,7 @@ function PagHome() {
       reader.onloadend = () => {
         const imageUrl = reader.result;
         setUserAvatar(imageUrl);
-        localStorage.setItem('userAvatar', imageUrl); 
+        localStorage.setItem('userAvatar', imageUrl);
       };
       reader.readAsDataURL(file);
     }
@@ -142,29 +187,29 @@ function PagHome() {
       )}
       <div className={`sidebar ${!sidebarVisible ? "hidden" : isMobile ? "overlay" : ""}`}>
         <div className="sidebar-top-buttons">
-      
-      
-      
-         
-         
-            <div className="btnSair" onClick={() => navigate("/PagPerfil")}>
-             <img src={OptionIcon} />
-            </div>
-             
-            <div className="btnNotificacao">
-              <Notificacao />
-            </div>
-         
-         
 
-          <div className="btnSair" onClick={() => {handleLogOut(navigate);}}>
-              <div id="DivNotificação">
-                <img src={IconLogOut} />
-              </div>
+
+
+
+
+          <div className="btnSair" onClick={() => navigate("/PagPerfil")}>
+            <img src={OptionIcon} />
+          </div>
+
+          <div className="btnNotificacao">
+            <Notificacao />
           </div>
 
 
-       
+
+          <div className="btnSair" onClick={() => { handleLogOut(navigate); }}>
+            <div id="DivNotificação">
+              <img src={IconLogOut} />
+            </div>
+          </div>
+
+
+
         </div>
         <div className="user-info">
           <div className="user-avatar" onClick={() => document.getElementById('avatarUpload').click()}>
@@ -215,14 +260,14 @@ function PagHome() {
             <img src={buttons[2].img} alt={buttons[2].title} className="button-image" />
             <span className="button-title">{buttons[2].title}</span>
           </button>
-        {showPontoPedido && (
+          {showPontoPedido && (
             <div className="estoque-options">
               <button className="option-button" onClick={() => navigate("/PagPontoPedido")}>Ponto de pedido</button>
               <button className="option-button" onClick={() => navigate("/PagCadFornecedor")}>Cadastro de fornecedor</button>
               <button className="option-button" onClick={() => navigate("/PagPesquisaFornecedor")}>Pesquisa de produtos</button>
             </div>
           )}
-            
+
           {buttons.slice(3).map((button, index) => (
             <button key={index} className="menu-button" onClick={() => navigate(button.link)}>
               <img src={button.img} alt={button.title} className="button-image" />
@@ -230,7 +275,7 @@ function PagHome() {
             </button>
           ))}
 
-        
+
 
         </div>
       </div>
@@ -246,10 +291,9 @@ function PagHome() {
           {sections.map((section, index) => (
             <div
               key={index}
-              className={`grid-item ${
-                section.title === "Evolução de Vendas" ? "evolucao-vendas" :
-                section.title === "Linha de Produto" ? "linha-produto" : ""
-              }`}
+              className={`grid-item ${section.title === "Evolução de Vendas" ? "evolucao-vendas" :
+                  section.title === "Linha de Produto" ? "linha-produto" : ""
+                }`}
             >
               <h2>{section.title}</h2>
               <p>{section.content}</p>
