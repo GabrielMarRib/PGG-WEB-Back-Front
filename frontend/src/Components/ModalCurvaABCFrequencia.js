a9c41c2ac7706b0f1a647f9f1e40bd3b33f30000
Deivyd Lima <ryuxzzzz@gmail.com>
2024-10-27 17:01:55 -0300

    CurvaABC atualizada com porcentagens de classificação alteráveis que ficam salvas no localStorage

---
+++ frontend/src/Components/ModalCurvaABCFrequencia.js
@@ -0,0 +1,36 @@
+import '../Styles/App/Service/ModalCurvaABC.css';
+
+const ModalCurvaABCFrequencia = ({isOpen, onClose, classeA, setClasseA, classeB, setClasseB, handleChangeClassificao}) => {
+    if (!isOpen) return null; // Retorna nada se o modal não estiver aberto
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button className="close-button" onClick={onClose}>Fechar</button>
+        <h2>Configurar Classificação</h2>
+        <label>
+          Limite A (%):
+          <input
+            type="number"
+            value={classeA}
+            onChange={(e) => handleChangeClassificao(Number(e.target.value), setClasseA)}
+            min="0"
+            max="100"
+          />
+        </label>
+        <label>
+          Limite B (%):
+          <input
+            type="number"
+            value={classeB}
+            onChange={(e) => handleChangeClassificao(Number(e.target.value), setClasseB)}
+            min="0"
+            max="100"
+          />
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default ModalCurvaABCFrequencia;
---
+++ frontend/src/Components/ModalCurvaABCValor.js
@@ -0,0 +1,36 @@
+import '../Styles/App/Service/ModalCurvaABC.css';
+
+const ModalCurvaABCValor = ({isOpen, onClose, limiteA, setLimiteA, limiteB, setLimiteB, handleChangeClassificao}) => {
+    if (!isOpen) return null; // Retorna nada se o modal não estiver aberto
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button className="close-button" onClick={onClose}>Fechar</button>
+        <h2>Configurar Classificação</h2>
+        <label>
+          Limite A (%):
+          <input
+            type="number"
+            value={limiteA}
+            onChange={(e) => handleChangeClassificao(Number(e.target.value), setLimiteA)}
+            min="0"
+            max="100"
+          />
+        </label>
+        <label>
+          Limite B (%):
+          <input
+            type="number"
+            value={limiteB}
+            onChange={(e) => handleChangeClassificao(Number(e.target.value), setLimiteB)}
+            min="0"
+            max="100"
+          />
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default ModalCurvaABCValor;
