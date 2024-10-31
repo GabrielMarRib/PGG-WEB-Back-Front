af000e99444efade3666e0e90653f124c73b0436
MIGUELLIMA11 <guelrider@gmail.com>
2024-10-30 23:21:09 -0300

    thiago ta gravido

--- frontend/src/Components/PEPS/PEPS.jsx
+++ frontend/src/Components/PEPS/PEPS.jsx
@@ -1,66 +1,252 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function PEPS({ produto }) {
+  const [produtosProximosVencimento, setProdutosProximosVencimento] = useState([]);
+
+  useEffect(() => {
+    const obterProdutosProximosVencimento = async () => {
+      try {
+        const response = await axios.post('http://pggzettav3.mooo.com/api/index.php', {
+          funcao: 'verificaProdutosProximosVencimento',
+          senha: '@7h$Pz!q2X^vR1&K'
+        });
+
+        if (response.data.message) {
+          setProdutosProximosVencimento(response.data.message);
+        }
+      } catch (error) {
+        console.log("Erro ao obter produtos próximos ao vencimento: " + error);
+      }
+    };
+
+    obterProdutosProximosVencimento();
+  }, []);
+
   // Estrutura do calendário manual para o mês
-  const dias = [
-    1, 2, 3, 4, 5, 6, 7,
-    8, 9, 10, 11, 12, 13, 14,
-    15, 16, 17, 18, 19, 20, 21,
-    22, 23, 24, 25, 26, 27, 28,
-    29, 30, 31,
-  ];
+  const diaValidade = produto && produto?.dataVenda && produto.dataVenda.split("-")[2] //O gostoso do thiago pediu pra por isso aqui: Teoricamente deveria ter ano e mes na validade também, mas fodasi (nois q voa bruxao)
+
+  const diaCompra = produto && produto?.dataCompra && produto.dataCompra.split("-")[2]
+  const mesCompra = produto && produto?.dataCompra && produto.dataCompra.split("-")[1]
+  const AnoCompra = produto && produto?.dataCompra && produto.dataCompra.split("-")[0]
+  const bissexto = (0 == AnoCompra % 4) && (0 != AnoCompra % 100) || (0 == AnoCompra % 400)
+  const meses = {
+    "01": "Janeiro",
+    "02": "Fevereiro",
+    "03": "Março",
+    "04": "Abril",
+    "05": "Maio",
+    "06": "Junho",
+    "07": "Julho",
+    "08": "Agosto",
+    "09": "Setembro",
+    "10": "Outubro",
+    "11": "Novembro",
+    "12": "Dezembro"
+  }
+  const dias = {
+    "01": [1, 2, 3, 4, 5, 6, 7,
+      8, 9, 10, 11, 12, 13, 14,
+      15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28,
+      29, 30, 31],
+    "02": [1, 2, 3, 4, 5, 6, 7,
+      8, 9, 10, 11, 12, 13, 14,
+      15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28,
+      bissexto ? 29 : null],
+    "03": [1, 2, 3, 4, 5, 6, 7,
+      8, 9, 10, 11, 12, 13, 14,
+      15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28,
+      29, 30, 31],
+    "04": [1, 2, 3, 4, 5, 6, 7,
+      8, 9, 10, 11, 12, 13, 14,
+      15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28,
+      29, 30],
+    "05": [1, 2, 3, 4, 5, 6, 7,
+      8, 9, 10, 11, 12, 13, 14,
+      15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28,
+      29, 30, 31],
+    "06": [1, 2, 3, 4, 5, 6, 7,
+      8, 9, 10, 11, 12, 13, 14,
+      15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28,
+      29, 30],
+    "07": [1, 2, 3, 4, 5, 6, 7,
+      8, 9, 10, 11, 12, 13, 14,
+      15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28,
+      29, 30, 31],
+    "08": [1, 2, 3, 4, 5, 6, 7,
+      8, 9, 10, 11, 12, 13, 14,
+      15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28,
+      29, 30, 31],
+    "09": [1, 2, 3, 4, 5, 6, 7,
+      8, 9, 10, 11, 12, 13, 14,
+      15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28,
+      29, 30],
+    "10": [1, 2, 3, 4, 5, 6, 7,
+      8, 9, 10, 11, 12, 13, 14,
+      15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28,
+      29, 30, 31],
+    "11": [1, 2, 3, 4, 5, 6, 7,
+      8, 9, 10, 11, 12, 13, 14,
+      15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28,
+      29, 30],
+    "12": [1, 2, 3, 4, 5, 6, 7,
+      8, 9, 10, 11, 12, 13, 14,
+      15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28,
+      29, 30, 31],
+  };
 
   // Função que renderiza os dias do calendário
+  const pegaCor = (dia) => {
+    if (dia == diaCompra)
+      return styles.diaCompra
+    else if(dia == diaValidade)
+      return styles.diaValidade
+    else return styles.day
+  } 
   const renderCalendar = () => {
-    return dias.map(day => (
-      <div key={day} style={styles.day}>{day}</div>
-    ));
+    return (
+      <div style={styles.calendarContainer}>
+        <h2 style={styles.title}>{meses[mesCompra]}</h2>
+        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(7, 1fr)', gap: '5px' }}>
+          {dias[mesCompra].map(day => (
+
+            <div key={day} style={pegaCor(day)}>{day}</div>
+          ))}
+        </div>
+      </div>
+    )
+
   };
 
   // Definindo estilos inline
   const styles = {
+    container: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      padding: '20px',
+      backgroundColor: '#f9f9f9',
+      minHeight: '100vh',
+    },
     calendarContainer: {
-      position: 'absolute',
-      bottom: '50px',
-      left: '20px',
-      backgroundColor: '#f0f0f0',
-      padding: '10px',
-      border: '1px solid #ccc',
-      borderRadius: '10px',
-      width: '400px', // Ajuste este valor conforme necessário para o seu design
-      boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
-      display: 'grid',                // Use grid para organização
-      gridTemplateColumns: 'repeat(7, 1fr)', // Define 7 colunas
-      gap: '5px',                    // Adiciona espaço entre os itens
+      flex: 1,
+      marginRight: '10px', // Reduzido para dar espaço ao outro contêiner
+      backgroundColor: '#fff',
+      padding: '15px',
+      borderRadius: '8px',
+      boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
     },
     day: {
-      padding: '5px',                // Reduzindo o padding das células
+      padding: '10px',
       border: '1px solid #ccc',
       textAlign: 'center',
       borderRadius: '3px',
-      backgroundColor: '#fff',
-      fontSize: '16px',              // Mantendo o tamanho de texto
-      margin: '2px',                 // Margem entre os dias
-      height: '50px',                // Altura fixa para os dias
+      backgroundColor: '#f0f0f0',
+      fontSize: '16px',
+      margin: '2px',
     },
     title: {
       textAlign: 'center',
-      fontSize: '18px',              // Ajuste do tamanho do título
+      fontSize: '20px',
       marginBottom: '10px',
     },
+    produtosContainer: {
+      flex: 1,
+      marginLeft: '10px', // Reduzido para dar espaço ao outro contêiner
+      backgroundColor: '#fff',
+      padding: '15px',
+      borderRadius: '8px',
+      boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
+    },
+    alerta: {
+      color: 'red',
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+    produtoItem: {
+      padding: '10px',
+      borderBottom: '1px solid #eee',
+    },
+    produtoVencido: {
+      color: 'red',
+      fontWeight: 'bold',
+    },
+    produtoProximo: {
+      color: 'orange',
+      fontWeight: 'bold',
+    },
+
+    diaCompra:  {
+      color:"green",
+      padding: '10px',
+      border: '1px solid #ccc',
+      textAlign: 'center',
+      borderRadius: '3px',
+      backgroundColor: '#A6FF7E',
+      fontSize: '16px',
+      margin: '2px',
+    },
+    diaValidade:{
+      color:"red",
+      padding: '10px',
+      border: '1px solid #ccc',
+      textAlign: 'center',
+      borderRadius: '3px',
+      backgroundColor: '#FF7E7E',
+      fontSize: '16px',
+      margin: '2px',
+    }  
   };
 
-function PEPS({produto}) {
   return (
-    <>
-    
-      <div style={styles.calendarContainer}>
-        <h2 style={styles.title}>Janeiro</h2>
-        {renderCalendar()}
+    <div style={styles.container}>
+      {
+        produto && renderCalendar()
+      }
+      <button onClick={() => console.log(mesCompra)}>Sexo</button>
+      <div style={styles.produtosContainer}>
+        <p className="card-title">Produtos que Vencem em Até 30 Dias.</p>
+        <p className="contador">
+          {produtosProximosVencimento.filter(produto => new Date(produto.dt_validade) < new Date()).length} Produtos Vencidos.
+        </p>
+        {produtosProximosVencimento.length > 0 ? (
+          <div className="produtos-vencimento">
+            <ul>
+              {produtosProximosVencimento.map(produto => {
+                const produtoVencido = new Date(produto.dt_validade) < new Date();
+                return (
+                  <li key={produto.numerolote} className="produto-item">
+                    <div className="produto-nome" style={{ color: produtoVencido ? 'red' : 'inherit' }}>
+                      {produto.nome}
+                    </div>
+                    <div className="produto-detalhes">
+                      <span className={produtoVencido ? "produto-vencido" : ""} style={{ color: produtoVencido ? 'red' : 'inherit' }}>
+                        Vencimento em: <strong>{new Intl.DateTimeFormat('pt-BR').format(new Date(produto.dt_validade))}</strong>
+                      </span>
+                      <span> Lote: <strong>{produto.numerolote}</strong></span>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        ) : (
+          <p className="alerta">Não há produtos próximos ao vencimento.</p>
+        )}
       </div>
-      <button onClick={()=>console.log(produto)}>Sexo</button>
-    </>
+    </div>
   );
 }
-}
+
 export default PEPS;
