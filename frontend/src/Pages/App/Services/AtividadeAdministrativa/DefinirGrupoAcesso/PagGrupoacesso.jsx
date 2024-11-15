ac22737551cbbde6b6f16d3f92036f6ee858b38b
GuilhermeTiago22 <guilherme.tiago06@gmail.com>
2024-11-15 20:31:10 -0300

    Pega ai filho

--- frontend/src/Pages/App/Services/AtividadeAdministrativa/DefinirGrupoAcesso/PagGrupoacesso.jsx
+++ frontend/src/Pages/App/Services/AtividadeAdministrativa/DefinirGrupoAcesso/PagGrupoacesso.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Titulo from '../../../../../Components/Titulo/Titulo';
+import CabecalhoHome from '../../../../../Components/Cabecalho/CabecalhoHome';
+import { useNavigate } from 'react-router-dom';
 
 function PagGrupoacesso() {
+
+  const navigate = useNavigate();
+  
   return (
-    <div>PagGrupoacesso</div>
-  )
+    <div className="PagPerfil">
+      <CabecalhoHome />
+      <Titulo tituloMsg='Gerenciar Grupos de Acesso' />
+
+      <button className="voltar" onClick={() => {navigate("/PagHome");}}>Voltar</button>
+      
+    </div>
+  );
 }
 
-export default PagGrupoacesso
+export default PagGrupoacesso;
