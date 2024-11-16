c69e1d8495d0be63df06e9a4c606ced135fedc6b
GabrielMarRib <gabrielsamer@gmail.com>
2024-11-15 22:43:02 -0300

    Pagina Lotes Finalizada

---
+++ frontend/src/Components/Modais/LoteModal/ModalAdicionarLote.js
@@ -0,0 +1,218 @@
+import React, { useState, memo, useEffect } from 'react';
+import './ModalLote.css';
+import axios from 'axios';
+import { useAlerta } from "../../../Context/AlertaContext.js";
+import AlertaNotificação from "../../NotificacaoAlert/AlertaNotificação.js";
+import ConfirmaModal from '../ConfirmaModal/ConfirmaModal.js';
+import { Link } from 'react-router-dom';
+
+const LoteMemo = memo(function ModalAddLote({ Produto, fechar }) { // teoricamente faria não ter reRender, mas ta tendo, ou seja, fds
+
+  const { Alerta } = useAlerta();
+
+  const [fornecedor, setFornecedor] = useState('');
+  const [FornecedorSelect, setFornecedorSelect] = useState([]);
+  const [dataCompra, setDataCompra] = useState('');
+  const [dataValidade, setDataValidade] = useState('');
+  const [valorCompra, setValorCompra] = useState('');
+  const [valorVenda, setValorVenda] = useState('');
+  const [quantidade, setQuantidade] = useState(0);
+
+
+  const pegarTodosFornecedores = async () => {
+    try {
+      const response = await axios.post('http://pggzettav3.mooo.com/api/index.php', {  // acessa via post (SEMPRE SERÁ POST)                
+        funcao: 'pegarTodosFornecedores', // dita qual função deve ser utilizada da api. (a gente te fala o nome) // ---> parâmetros da consulta... SÃO necessários.
+        senha: '@7h$Pz!q2X^vR1&K' // teoricamente essa senha tem q ser guardada em um .env, mas isso é trabalho do DEIVYD :)
+      });
+      setFornecedorSelect(response.data.fornecedores); // coloca a LISTA de categorias em uma useState
+      console.log(response.data) // log para sabermos o que foi pego
+    } catch (error) {
+      console.log("deu ruim: " + error) // log para sabermos qual foi o erro
+    }
+  };
+  useEffect(() => {
+    pegarTodosFornecedores();
+  }, [])
+
+
+
+
+
+  const CadastrarLote = async (e) => {
+    
+    // setShowConfirma(true);
+    if(fornecedor == '' || dataCompra == '' || dataValidade == '' || quantidade == 0 || valorCompra == '' || valorVenda == ''){
+      Alerta(1, "Preencha todos os valores");
+      return
+    }
+
+
+   
+    try {
+        const response = await axios.post('http://pggzettav3.mooo.com/api/index.php', {
+          funcao: 'inserelote', 
+          senha: '@7h$Pz!q2X^vR1&K',
+          dt_compra: dataCompra,
+          dt_validade: dataValidade,
+          qtde: quantidade,
+          vlr_compra: valorCompra,
+          vlr_venda: valorVenda,
+          produto: Produto.id_produtos,
+          fornecedor: fornecedor.id_fornecedor,
+        });
+        console.log(response.data) 
+        Alerta(2, "Cadastrado!");
+        setFornecedor('')
+        setDataCompra('')
+        setValorCompra('')
+        setValorVenda('')
+        setQuantidade(0)
+        setDataValidade('')
+        fechar()
+      } catch (error) {
+        Alerta(3, "Ocorreu um problema!");
+        console.log("Não cadastrou: " + error) 
+      }
+
+  
+
+
+  }
+
+
+
+
+const handleChangeFornecedor = (e) => {
+    const val = e.target.value
+    if (val === 'Vazio') {
+      setFornecedor('')
+    } else {
+    setFornecedor(JSON.parse(val));
+    }
+   
+
+  };
+  const MapearFornecedor = (Fornecedor) => {
+    return (
+      <option value={JSON.stringify(Fornecedor)}>{Fornecedor.id_fornecedor} - {Fornecedor.nome}</option>
+    );
+  }
+
+  return (
+    <div className='ModalAtualizarLote'>
+        <AlertaNotificação />
+    <div className='conteudo-modal'>
+      <div className='cabecalho-modal'>
+      <h1>Cadastrar novo Lote</h1>
+      <button onClick={fechar} className='botao-fechar'>X</button>
+      </div>
+
+    <center><h1> {Produto.nome} </h1></center>
+    
+  
+
+
+
+    <form className="formCat" onSubmit={(e) => {e.preventDefault(); CadastrarLote();}}>
+      
+                    Fornecedor:
+                    <br />
+                    <select
+                      onChange={handleChangeFornecedor}
+                      value={fornecedor}
+                    >
+                      <option value="Vazio">Selecione um fornecedor</option>
+                      <option value="Vazio">Sem fornecedor</option>
+                      {Array.isArray(FornecedorSelect) ? (
+                        FornecedorSelect?.map(MapearFornecedor)
+                      ) : (
+                        null
+                      )
+
+
+                      }
+                    </select>
+                    
+                    <br />
+                    Data de Compra: <span style={{ color: "red" }}> *</span>
+                    <input
+                      type="date"
+                      id="dataCompra"
+                      rows="3"
+                      required value={dataCompra}
+                      onChange={(e) => setDataCompra(e.target.value)}
+                    />
+               
+                  
+                    Data de Validade:
+                    <input
+                      type="date"
+                      id="dataVenda"
+                      rows="3"
+                      value={dataValidade}
+                      onChange={(e) => setDataValidade(e.target.value)}
+                    />
+             
+
+                
+                    Quantidade: <span style={{ color: "red" }}> *</span>
+                    <input
+                      type="int"
+                      id="quantidadeProduto"
+                      required value={quantidade}
+                      onChange={(e) => setQuantidade(e.target.value === '' ? 0 : parseInt(e.target.value))}
+                    />
+                
+
+
+                 
+                    Valor da Compra (Unitário):
+                    <input
+                      type="number"
+                      id="valorCompra"
+                      required value={valorCompra}
+                      onChange={(e) => {
+                        const value = e.target.value;
+                        if (/^\d*\.?\d*$/.test(value)) {
+                          setValorCompra(value);
+                        }
+                      }}
+                    />
+              
+
+                 
+                  
+                      Valor da Venda (Unitário): <span style={{ color: "red" }}> *</span>
+                
+                    <input
+                      type="number"
+                      id="valorVenda"
+                      required value={valorVenda}
+                      onChange={(e) => {
+                        const value = e.target.value;
+                        if (/^\d*\.?\d*$/.test(value)) {
+                          setValorVenda(value);
+                        }
+                      }}
+                    />
+             
+
+
+
+      
+      <button className='botao-testar'>Cadastrar</button>
+
+
+      </form>
+
+    </div>
+
+    </div>
+
+
+  )
+
+});
+
+export default LoteMemo;
--- frontend/src/Pages/App/Services/Estoque/GerirLotes/GerirLotes.js
+++ frontend/src/Pages/App/Services/Estoque/GerirLotes/GerirLotes.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from "react";
 import CabecalhoHome from "../../../../../Components/Cabecalho/CabecalhoHome.js";
-//import "../../../Styles/App/Service/PagAddCategoria.css"; ???????????????????????
+import "../GerirLotes/GerirLotes.css"
 
 import axios from "axios";
 import { CheckCamposVazios } from "../../../../../Functions/Functions.js";
@@ -9,12 +9,16 @@ import { useNavigate } from "react-router-dom";
 import AlertaNotificação from "../../../../../Components/NotificacaoAlert/AlertaNotificação.js";
 import InfoModalCat from "../../../../../Components/Modais/CategoriaModal/InfoModalCat.js";
 import ModalAtualizarLote from "../../../../../Components/Modais/LoteModal/ModalAtualizarLote.js";
+import ModalAddLote from "../../../../../Components/Modais/LoteModal/ModalAdicionarLote.js";
 import { useAlerta } from "../../../../../Context/AlertaContext.js";
 import { useContext } from "react";
 import { UserContext } from "../../../../../Context/UserContext.js";
 import Titulo from "../../../../../Components/Titulo/Titulo.jsx";
 
 import Box from "../../../../../Assets/Box.png";
+import IconProduto from "../../../../../Assets/IconProdutoUnico.png";
+import IconAddLote from "../../../../../Assets/IconAddLote.png";
+import IconSeta from "../../../../../Assets/IconSeta.png";
 import BtnAjuda from "../../../../../Components/BotaoAjuda/BtnAjuda.js";
 import BuscaCategorias from "../../../../../Components/BuscaCategoria/BuscaCategoriasComponente.js";
 
@@ -23,17 +27,23 @@ function GerirLotes() {
   const navigate = useNavigate();
 
   const [TabelaLote, SetTabelaLote] = useState([]); // Inicializa como array vazio
+  const [TabelaProdutos, SetTabelaProdutos] = useState([]);
   const [ShowModal, setShowModal] = useState(false);
+  const [ShowModalAddLote, setShowModalAddLote] = useState(false);
   const [LoteSelecionado, setLoteSelecionado] = useState(false);
   const [FiltroLotes, setFiltroLotes] = useState(null);
+  const [FiltroProdutos, setFiltroProdutos] = useState(null);
   const [InputFiltro, setInputFiltro] = useState(null);
   const [FiltroCategoria, setFiltroCategoria] = useState(null);
-  const [PesquisaProdutos, setPesquisaProdutos] = useState(null);
+  const [ProdutoSelecionado, setProdutoSelecionado] = useState(null);
+  
+  
+  
   
   const PegarLotes = async (SetTabelaLote) => {
     try {
       console.log("Pegando lotes");
-      console.log("User" + JSON.stringify(User.id));
+ 
 
       const response = await axios.post(
         "http://pggzettav3.mooo.com/api/index.php",
@@ -50,10 +60,32 @@ function GerirLotes() {
     }
   };
 
+
+  const PegarProdutos = async (SetTabelaProdutos) => {
+    try {
+      console.log("Pegando produtos");
+
+
+      const response = await axios.post(
+        "http://pggzettav3.mooo.com/api/index.php",
+        {
+          funcao: "pegadados",
+          senha: "@7h$Pz!q2X^vR1&K",
+        }
+      );
+
+      console.log(response.data);
+      SetTabelaProdutos(response.data);
+    } catch (error) {
+      console.log("Não pegou: " + error);
+    }
+  };
+
   //Após iniciar a pagina, rodar a função que pega os dados da tabela
   useEffect(() => {
     const UssseEffect = async () => {
       await PegarLotes(SetTabelaLote);
+      await PegarProdutos(SetTabelaProdutos);
     };
     UssseEffect();
   }, []);
@@ -67,40 +99,45 @@ function GerirLotes() {
 
   const handleFecharModal = async (bool) => {
     setShowModal(bool);
+    setShowModalAddLote(bool);
+    setProdutoSelecionado(null)
     await PegarLotes(SetTabelaLote);
+    await PegarProdutos(SetTabelaProdutos);
   };
 
   const [showPopup, setShowPopup] = useState(false);
 
   const MapearLotes = (item) => {
     //Mapeando item por item dentro da tabela lote
+  
     return (
       <div key={item.numerolote} className="DivGerirLotes">
-        <center>
-          <img src={Box} className="IconBoxImg" alt="Imagem Externa" />
-          <p className="p" style={{ fontSize: "20px" }}>
-            <strong> Id do lote: </strong> {item.numerolote}
-          </p>
-        </center>
-        <center>
-          <p className="p" style={{ fontSize: "20px", marginTop: "0px" }}>
-            {" "}
-            <strong> Produto: </strong>{item.nome}{" "}
-          </p>
-        </center>
-        <center>
-          <p className="p" style={{ fontSize: "20px", marginTop: "0px" }}>
-            {" "}
-            <strong>Codigo do Fornecedor: </strong> {item.fornecedor ? item.fornecedor : "Sem fornecedor"}
-          </p>
-        </center>
-        <center>
-          <p className="p"> <strong> Data de compra:   </strong>{item.dt_compra} /</p>
-          <p className="p"> <strong> Data de validade: </strong>{item.dt_validade} /</p>
-          <p className="p"> <strong> Valor da venda:   </strong>{item.vlr_venda} /</p>
-          <p className="p"> <strong> Valor da compra: </strong>{item.vlr_compra} /</p>
+<div style={{display: "inline-flex", flexDirection: "row", rowGap: "0px"}}>
+  <div style={{display: "inline-flex"}}>
+
+            <img src={Box} className="IconBoxImg2" alt="Imagem Externa" />
+
+        <div className="LotePart1">
+                  <p className="p" style={{ fontSize: "15px", marginTop: "0px" }}>
+                    <strong> Id do lote: </strong> {item.numerolote}
+                  </p>
+                  <p className="p" style={{ fontSize: "15px", marginTop: "-10px"}}>
+                    <strong> Produto: </strong>{item.nome}
+                  </p>
+                 <p className="p" style={{ fontSize: "15px", marginTop: "-10px"}}>
+                   <strong>Codigo do Fornecedor: </strong> {item.fornecedor ? item.fornecedor : "Sem fornecedor"}
+                 </p>
+        </div>
+  </div>
+      
+     
+       <div className="DivDadosLote">
+          <p className="pp"> <strong> Data de compra:   </strong>{item.dt_compra} / <strong> Data de validade: </strong>{item.dt_validade}</p>
+          <p className="p"> <strong> Valor da venda:   </strong>{item.vlr_venda} / <strong> Valor da compra: </strong>{item.vlr_compra}</p>
           <p className="p"> <strong> Valor da compra: </strong>{item.vlr_compra} </p>
-        </center>
+       </div>
+
+    </div>
 
         <div>
           <div className="divbtnEditarGerirLotes">
@@ -112,81 +149,124 @@ function GerirLotes() {
               Editar
             </button>
           </div>
+
         </div>
       </div>
     );
   };
-  const FiltraPorCategoria = async () => {
-    try {
-      const response = await axios.post(
-        "http://pggzettav3.mooo.com/api/index.php",
-        {
-          funcao: "pegaprodutosporcategoria",
-          senha: "@7h$Pz!q2X^vR1&K",
-          codcategoria: FiltroCategoria.id_categorias
-        }
-      );
-      console.log(response.data);
-    
-      let ProdutosDaCategoria = response.data
-      const FiltrarLotes = TabelaLote.filter((Lot) =>
-        ProdutosDaCategoria.some((Prod) => Prod.id_produtos === Lot.produto)
-      );
-
-      setFiltroLotes(FiltrarLotes)
-    } catch (error) {
-      console.log("Não pegou: " + error);
+  const ShowLotes = (Item) =>{
+    if(Item == ProdutoSelecionado){
+      setProdutoSelecionado(null)
+      return
     }
+    setProdutoSelecionado(Item)
+    const LotesFiltrados2 = TabelaLote.filter((lote) => lote.produto === Item.id_produtos);
+    setFiltroLotes(LotesFiltrados2);
+
+
+  } 
+
+ 
+
+
+  const MapearProdutos = (item) => {
+    //Mapeando item por item dentro da tabela lote
+   
+    
+    return (
+      <div key={item.numerolote} className="DivGeralProdutosELotes">
+                <div key={item.numerolote} className="DivGerirProdutos" >
+                <div onClick={() => ShowLotes(item)} style={{ cursor: "pointer"}}>
+
+                  <div style={{ display: "inline-block"}}>
+                  <div style={{ display: "flex", flexDirection: "column", marginTop: "15px", marginLeft: "10px", width: "500px"}}>
+                      <img src={IconProduto} className="IconBoxImg" alt="Imagem Externa" />
+
+                              <div style={{ display: "flex", flexDirection: "column", marginLeft: "44px", marginTop: "-35px", width: "500px"}}>
+                                      <p className="p" style={{fontSize: "20px", marginTop: "0px",marginLeft: "15px" }}>
+                                        <strong> Produto: </strong> {item.nome} 
+                                      </p>
+
+                                  <p className="p" style={{fontSize: "20px", marginLeft: "0", marginTop: "0px", marginLeft: "15px"}}>
+                                    <strong> Id do Produto: </strong> {item.id_produtos}
+                                  </p>
+                              </div>
+                            </div>
+                            </div>
+
+
+                        <div className="DivImgSeta">
+                          <div >
+                            <img src={IconSeta} className="IconBoxImg" alt="Imagem Externa"/>
+                          </div>
+                        </div>
+                  </div>
+                      
+                {ProdutoSelecionado?.id_produtos === item.id_produtos && (
+                  <>
+                    <center><hr className="HR"/></center>
+                    {FiltroLotes.map(MapearLotes)}
+
+
+                    <div className="DivAdicionarLote" onClick={() => setShowModalAddLote((prev) => !prev)}>
+                    <img src={IconAddLote} className="IconBoxImg3" alt="Imagem Externa" />
+                    <h2>Adicionar lote</h2>
+                    </div>
+                  </>
+                )}
+
+
+
+                </div>
+
+
+
+  </div>
+    );
+  };
+  const FiltraPorCategoria = async () => {
+    
+    
+      console.log("Entrou aqui")
+      console.log(FiltroCategoria)
+      const FiltrarProdutos2 = TabelaProdutos.filter((Prod) => Prod.categoria == FiltroCategoria?.id_categorias );
+      setFiltroProdutos(FiltrarProdutos2)
+   
   } 
   const FiltraPorCategoriaEInput = async () => {
-    try {
-      const response = await axios.post(
-        "http://pggzettav3.mooo.com/api/index.php",
-        {
-          funcao: "pegaprodutosporcategoria",
-          senha: "@7h$Pz!q2X^vR1&K",
-          codcategoria: FiltroCategoria.id_categorias
-        }
-      );
-      console.log(response.data);
-      let ProdutosDaCategoria = response.data
-      const FiltrarLotes = TabelaLote.filter((Lot) =>
-        ProdutosDaCategoria.some((Prod) => Prod.id_produtos === Lot.produto)
-      );
   
-      const FiltroLote = FiltrarLotes.filter((Lote) => {
+    const FiltrarProdutosCat = TabelaProdutos.filter((Prod) => Prod.categoria == FiltroCategoria?.id_categorias );
+  
+      const FiltroProduto = FiltrarProdutosCat.filter((Prod) => {
         if(isNaN(InputFiltro)){
-          return Lote.nome.toLowerCase().includes(InputFiltro.toLowerCase()) //não é num; vê o nome
+          return Prod.nome.toLowerCase().includes(InputFiltro.toLowerCase()) //não é num; vê o nome
         }else{
-          return Lote
+          return Prod
         }
       })
-      setFiltroLotes(FiltroLote)
-    } catch (error) {
-      console.log("Não pegou: " + error);
-    }
+      setFiltroProdutos(FiltroProduto)
 
   }
 
   useEffect( () => {
+    //FILTROSSS
   
     if(InputFiltro == ""){
       setInputFiltro(null)
     }
     if(InputFiltro == null && FiltroCategoria){
       //Só o filtro da categoria
-      console.log('Entro')
       FiltraPorCategoria()
     }else if(InputFiltro && FiltroCategoria == null){
       //Só o filtro padrão
-      const FiltroLote = TabelaLote.filter((Lote) => {
+      const FiltroProduto2 = TabelaProdutos.filter((Prod) => {
         if(isNaN(InputFiltro)){
-          return Lote.nome.toLowerCase().includes(InputFiltro.toLowerCase()) //não é num; vê o nome
+          return Prod.nome.toLowerCase().includes(InputFiltro.toLowerCase()) //não é num; vê o nome
         }else{
           return null
         }
       })
-      setFiltroLotes(FiltroLote)
+      setFiltroProdutos(FiltroProduto2)
     }else{
       //Os Dois filtros
       FiltraPorCategoriaEInput()
@@ -196,7 +276,7 @@ function GerirLotes() {
   
 
   return (
-    <div className="PagAddCategoria">
+    <div className="PagGerirLotes">
       <CabecalhoHome />
       <AlertaNotificação />
       <Titulo tituloMsg="Gerenciamento de lotes" />
@@ -242,6 +322,13 @@ function GerirLotes() {
         />
       ) : null}
 
+    {ShowModalAddLote ? (
+        <ModalAddLote
+          Produto={ProdutoSelecionado}
+          fechar={() => handleFecharModal(false)}
+        />
+      ) : null}
+
       <div className="Formulario">
         <div className="ListaCategorias">
           <div className="Filtro">
@@ -257,29 +344,14 @@ function GerirLotes() {
             </div>
             
           </div>
-          {/* <div className="barra-pesquisa">
-            </div>
-            <div className="teste">
-              <FiltragemCategoria
-                setFiltroSelecionado={setFiltroCategoriaSelecionada}
-                FiltroSelecionado={FiltroCategoria}
-              />
-            </div>
+         
 
-            <input
-              type="text"
-              placeholder="Pesquisar produto..."
-              value={pesquisaProduto}
-              onChange={(e) => setPesquisaProduto(e.target.value)}
-            />
-          </div> */}
-
-          {TabelaLote.length > 0 ? ( // Verifica se há dados no array antes de mapear sa bomba
+          {TabelaProdutos.length > 0 ? ( // Verifica se há dados no array antes de mapear sa bomba
              InputFiltro == null && FiltroCategoria == null  ? (
-               TabelaLote.map(MapearLotes)
+              TabelaProdutos.map(MapearProdutos)
              ) : (
-              FiltroLotes ? (
-                FiltroLotes.map(MapearLotes)
+              FiltroProdutos ? (
+                FiltroProdutos.map(MapearProdutos)
               ) : (
                 <>
                 Carregando ...
@@ -287,7 +359,7 @@ function GerirLotes() {
               )
              )
           ) : (
-            <p>Nenhum lote disponível.</p> // Mostra caso n tenha porra nenhuma no blg lá
+            <p>Nenhum lote disponível ou ocorreu um problema.</p> // Mostra caso n tenha porra nenhuma no blg lá
           )}
         </div>
       </div>
