780f732379fdb89951cf7bd0fade1e23dfc7d68e
Marcos Vinicius <marcosbayblade78@gmail.com>
2024-11-17 10:08:01 -0300

    teste

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './Styles/index.css';
 import App from './App';
 import { UserContextProvider } from './Context/UserContext'; // Import the UseProvider component from your context file
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
