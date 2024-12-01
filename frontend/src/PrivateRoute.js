7f8a7db3b947dcee598905484cca0549c0533840
Thiago <tguigomarques@gmail.com>
2024-12-01 13:36:23 -0300

    KKKKKKKKKKKKKKKKKKKKKKKKKKKK PERMISSOES EM TEMPO REAL MLK

--- frontend/src/PrivateRoute.js
+++ frontend/src/PrivateRoute.js
@@ -1,30 +1,45 @@
-// src/Components/PrivateRoute.js
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
-import { useContext } from 'react';
 import { UserContext } from './Context/UserContext';
 import { PermissoesContext } from './Context/PermissoesContext';
 import { checaPermissaoVisualizacao } from './Config/Permissoes';
+import { pegaPermissoesWHERE } from './Config/Permissoes';
 
-
-const PrivateRoute = ({ element: Component,  intent: Intent, ...rest }) => {
+const PrivateRoute = ({ element: Component, intent: Intent, ...rest }) => {
   const { User, isLoading } = useContext(UserContext);
-  const {Permissoes, isLoadingP} = useContext(PermissoesContext)
-
+  const { Permissoes, setPermissoes, isLoadingP } = useContext(PermissoesContext);
+  const [permissionsReady, setPermissionsReady] = useState(false);
   const location = useLocation();
 
-  if (isLoading || isLoadingP) {
-    return <div>Carregando...</div>
+  useEffect(() => {
+    if (User && !permissionsReady) {
+      const fetchPermissions = async () => {
+        console.log("useEFFECT")
+        console.log(Permissoes)
+        
+        const id_grupo = User?.userData?.Grupo_Acesso;
+        const permissoes_OBJ = await pegaPermissoesWHERE(id_grupo, true);
+        console.log(permissoes_OBJ)
+        if (permissoes_OBJ !== Permissoes) {
+          setPermissoes(permissoes_OBJ);
+        }
+        setPermissionsReady(true);
+      };
+
+      fetchPermissions();
+    }
+  }, [User, Permissoes, setPermissoes, permissionsReady]);
+
+  if (isLoading || isLoadingP || (!permissionsReady && Permissoes)) {
+    return <div>Carregando...</div>;
   }
 
   if (User) {
-    if(Permissoes){
-      //console.log((checaPermissaoVisualizacao(Permissoes.data, Permissoes.nome, intent)))
-       if(checaPermissaoVisualizacao(Permissoes.data, Permissoes.nome, Intent))
-        return Component;
-      else
-         return location.pathname !== "/PagHome" ? <Navigate to="/PagHome" state={{ from: location }}  /> : null;
-     }
+    if (Permissoes && checaPermissaoVisualizacao(Permissoes.data, Permissoes.nome, Intent)) {
+      return Component;
+    } else {
+      return location.pathname !== "/PagHome" ? <Navigate to="/PagHome" state={{ from: location }} /> : null;
+    }
   } else {
     return <Navigate to="/PagLogin" state={{ from: location }} />;
   }
