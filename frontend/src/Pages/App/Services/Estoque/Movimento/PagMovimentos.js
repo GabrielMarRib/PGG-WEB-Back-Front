ca401fde76584b076f596c24ddf74a9a63969b15
pedrocarrion <binhomyke@gmail.com>
2024-12-02 03:34:44 -0300

    n deixar ficar negativo hehe

--- frontend/src/Pages/App/Services/Estoque/Movimento/PagMovimentos.js
+++ frontend/src/Pages/App/Services/Estoque/Movimento/PagMovimentos.js
@@ -167,8 +167,8 @@ const PagMovimentos = () => {
           <td>{Movimento.valor}</td> {/* Custo unitario */}
           <td>{Movimento.qtde * Movimento.valor}</td>  {/* VALOR */}
           <td>{QuantidadeTotal}</td>
-          <td>{ValorTotal}</td>
-          <td>{CustoMedio.toFixed(2)}</td>
+          <td>{Math.abs(ValorTotal)}</td>
+          <td>{Math.abs(CustoMedio).toFixed(2)}</td>
         </tr>
       )
     } else {
@@ -194,8 +194,8 @@ const PagMovimentos = () => {
             <td>{Movimento.valor}</td> {/* Custo unitario */}
             <td>{Movimento.qtde * Movimento.valor}</td>  {/* VALOR */}
             <td>{QuantidadeTotal}</td>
-            <td>{ValorTotal}</td>
-            <td>{CustoMedio.toFixed(2)}</td>
+            <td>{Math.abs(ValorTotal)}</td>
+            <td>{Math.abs(CustoMedio).toFixed(2)}</td>
           </tr>
         )
       } else {
